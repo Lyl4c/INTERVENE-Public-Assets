575a0a99e05b44c98c987b384c2349f46731d2c9
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-03-05 23:34:19 -0800

    0.9.16

--- News/newsPosts.js
+++ News/newsPosts.js
@@ -1,44 +1,37 @@
-/*
-var postCount = 0
-var url = 'https://lyl4c.github.io/INTERVENE-Public-Assets/News/Posts/'
-var fileName = '-intervene'
-*/
-console.log('import successful');
-
-//News Post Crawler; Returns # of posts found in url param
-function loadNews(url,fileName,postCount=0) {
-  xhttp.open('GET', url+(postCount+1)+fileName+'.html', true);
-  xhttp.onreadystatechange = function() {
-    if(xhttp.readyState == XMLHttpRequest.DONE) {
-      if (xhttp.status) {
-        statusType = Math.floor(xhttp.status/100);
-        if(statusType == 2) {
-          postCount++;
-        } else if(statusType == 4) {
-          console.log('Number of Posts = '+postCount);
-          getNews(url,fileName,postCount);
-          return;
+let Lylac = {
+  
+  //News Post Crawler; Returns # of posts found in url param
+  loadNews: function(url,fileName,postCount=0) {
+    xhttp.open('GET', url+(postCount+1)+fileName+'.html', true);
+    xhttp.onreadystatechange = function() {
+      if(xhttp.readyState == XMLHttpRequest.DONE) {
+        if (xhttp.status) {
+          statusType = Math.floor(xhttp.status/100);
+          if(statusType == 2) {
+            postCount++;
+          } else if(statusType == 4) {
+            console.log('Number of Posts = '+postCount);
+            getNews(url,fileName,postCount);
+            return;
+          }
+        }
+        //Lylac.loadNews(url,fileName,postCount);
+      }
+    }
+    xhttp.send();
+  },
+  
+  getNews: function(url,fileName,postCount) {
+    for (let i=1;i<=5;i++) {
+      //grabs text files from url param
+      let iframe = insertDiv('#infobox #news',i,setDivAtt({'div':'iframe','class':'newsPost','style':'border:none;','scrolling':'no','height':'0'}));
+      //sets all iframes under #news to their individual content heights.
+      iframe.onload = function() {
+        if (!RegExp('Page not found').test(this.contentDocument.title)) {
+          this.height = this.contentWindow.document.body.scrollHeight;
         }
       }
-      loadNews(url,fileName,postCount);
+      iframe.src = url+(postCount-i+1)+fileName+'.html';
     }
   }
-  xhttp.send();
-}
-
-//loadNews();
-
-function getNews(url,fileName,postCount) {
-  for (let i=1;i<=5;i++) {
-    //grabs text files from url param
-    let iframe = insertDiv('#infobox #news',i,setDivAtt({'div':'iframe','class':'newsPost','style':'border:none;','scrolling':'no','height':'0'}));
-    //sets all iframes under #news to their individual content heights.
-    iframe.onload = function() {
-      if (!RegExp('Page not found').test(this.contentDocument.title)) {
-        this.height = this.contentWindow.document.body.scrollHeight;
-      }
-    }
-    iframe.src = url+(postCount-i+1)+fileName+'.html';
-  }
-  console.log('loaded news posts');
 }
