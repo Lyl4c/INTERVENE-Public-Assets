2cdfb5feed854e50e08ded156308490185d73489
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-03-05 17:31:48 -0800

    Update newsPosts.js

--- News/newsPosts.js
+++ News/newsPosts.js
@@ -26,19 +26,20 @@ function loadNews() {
 
 function getNews(postCount) {
   //grabs text files from var url in loadNews()
-  for (var i=(postCount-4);i<=postCount;i++) {
+  for (let i=(postCount-4);i<=postCount;i++) {
     //console.log(url+i+fileName+'.html');
     console.log('1st i = '+i);
     //console.log('postCount = '+(postCount-i+1));
     insertDiv('#infobox #news',1,setDivAtt({'div':'iframe','class':'newsPost','style':'border:none;','scrolling':'no','height':'0','src':url+i+fileName+'.html'}));
   }
-  i = 0;
-  console.log('between = '+i);
+  
+  //i = 0;
+  
   //sets all iframes under #news to their individual content heights.
   window.addEventListener('load', function(e) {
     console.log('retrieving news');
     var iFrame = document.querySelectorAll('.newsPost');
-    for(var i=0;i<iFrame.length;i++) {
+    for(let i=0;i<iFrame.length;i++) {
       console.log(i+' = '+(!RegExp('Page not found').test(iFrame[i].contentDocument.title)));
       console.log(i+' = '+iFrame[i].contentWindow.document.body.scrollHeight);
       if (!RegExp('Page not found').test(iFrame[i].contentDocument.title)) {
