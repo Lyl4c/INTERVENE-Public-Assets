4face43850f638c247f0f98c5b243e89e6449be8
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-03-05 17:49:46 -0800

    Update newsPosts.js

--- News/newsPosts.js
+++ News/newsPosts.js
@@ -26,15 +26,21 @@ function loadNews() {
 
 function getNews(postCount) {
   //grabs text files from var url in loadNews()
-  for (i=(postCount-4);i<=postCount;i++) {
+  for (let i=(postCount-4);i<=postCount;i++) {
+    //console.log(url+i+fileName+'.html');
+    console.log('1st i = '+i);
+    //console.log('postCount = '+(postCount-i+1));
     insertDiv('#infobox #news',1,setDivAtt({'div':'iframe','class':'newsPost','style':'border:none;','scrolling':'no','height':'0','src':url+i+fileName+'.html'}));
   }
-  
+  console.log('between = '+i);
   //sets all iframes under #news to their individual content heights.
   window.addEventListener('load', function(e) {
-    i=0;
+    console.log('retrieving news');
+    //i=0;
     var iFrame = document.querySelectorAll('.newsPost');
-    for(i=0;i<iFrame.length;i++) {
+    for(let i=0;i<iFrame.length;i++) {
+      console.log(i+' = '+(!RegExp('Page not found').test(iFrame[i].contentDocument.title)));
+      console.log(i+' = '+iFrame[i].contentWindow.document.body.scrollHeight);
       if (!RegExp('Page not found').test(iFrame[i].contentDocument.title)) {
         iFrame[i].height = iFrame[i].contentWindow.document.body.scrollHeight;
       }
