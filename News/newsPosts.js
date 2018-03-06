265914b76953b6878d235adcad6832d7388ba6e1
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-03-05 16:00:44 -0800

    Update newsPosts.js

--- News/newsPosts.js
+++ News/newsPosts.js
@@ -2,7 +2,6 @@ var postCount = 0
 var url = 'https://lyl4c.github.io/INTERVENE-Public-Assets/News/Posts/'
 var fileName = '-intervene'
 console.log('import successful');
-
 function loadNews() {
   xhttp.open('GET', url+(postCount+1)+fileName+'.html', true);
   xhttp.onreadystatechange = function() {
@@ -13,7 +12,7 @@ function loadNews() {
           postCount++;
         } else if(statusType == 4) {
           console.log('Number of Posts = '+postCount);
-          insertNews(postCount);
+          getNews(postCount);
           return;
         }
       }
@@ -25,22 +24,23 @@ function loadNews() {
 
 //loadNews();
 
-function insertNews(postCount) {
+function getNews(postCount) {
   //grabs text files from var url in loadNews()
   for (var i=(postCount-4);i<=(postCount);i++) {
     insertDiv('#infobox #news',1,setDivAtt({'div':'iframe','class':'newsPost','style':'border:none;','scrolling':'no','height':'0','src':url+i+'-intervene.html'}));
   }
-  fixNews();
-}
-
-function fixNews() {
+  
   //sets all iframes under #news to their individual content heights.
-  var iFrame = document.querySelectorAll('.newsPost');
-  console.log(iFrame);
-  for(var i=0;i<iFrame.length;i++) {
-    if (!RegExp('Page not found').test(iFrame[i].contentDocument.title)) {
+  window.addEventListener('load', function(e) {
+    console.log('retrieving news');
+    var iFrame = document.querySelectorAll('.newsPost');
+    for(var i=0;i<iFrame.length;i++) {
+      console.log(i+' = '+(!RegExp('Page not found').test(iFrame[i].contentDocument.title)));
       console.log(i+' = '+iFrame[i].contentWindow.document.body.scrollHeight);
-      iFrame[i].height = iFrame[i].contentWindow.document.body.scrollHeight;
+      if (!RegExp('Page not found').test(iFrame[i].contentDocument.title)) {
+        iFrame[i].height = iFrame[i].contentWindow.document.body.scrollHeight;
+      }
     }
-  }
+  });
+  console.log('loaded news posts');
 }
