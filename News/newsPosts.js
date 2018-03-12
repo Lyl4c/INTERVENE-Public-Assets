d567b82170534c6ab383b1e5792582926eb71ff0
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-03-11 23:59:53 -0700

    0.10.24
    finalizing + final testing

--- News/newsPosts.js
+++ News/newsPosts.js
@@ -1,12 +1,21 @@
+//News Post Script made by Lylac_SYS
+//Assistance provided by: sickhippie, Miroware
+//This can load News Posts from an external source if set up correctly.
 var Lylac = {
   //news setup
-  setupNews: function(selector,selectorChild) {
+  //STRING selector = target div | The parent div of where you want to put your posts.
+  //NUMBER selectorChild = child div of target div | Where you actually want to place your posts.
+  setupNews: function(selector,selectorChild=0) {
     let temp = document.createElement('div');
     temp.id = 'adventureNews';
     document.querySelector(selector).insertBefore(temp,document.querySelector(selector).childNodes[selectorChild]);
   },
   
   //News Post Crawler; Returns # of posts found in url param
+  //STRING url = web address | external source of your News Posts.
+  //STRING fileName = name of the file | example: -intervene | <- use this instead of the full name: 71-intervene.html.
+  //                                     The file MUST be an HTML file.
+  //NUMBER postCount = starting number of the crawler code | can be used to skip post entries.
   loadNews: function(url,fileName,postCount=0) {
     xhttp.open('GET', url+(postCount+1)+fileName+'.html', true);
     xhttp.onreadystatechange = function() {
@@ -19,31 +28,35 @@ var Lylac = {
             console.log('Number of Posts = '+postCount);
             Lylac.getNews(url,fileName,postCount);
             return;
+            //Recursion loop stops here.
           }
         }
+        //recursion loop
         Lylac.loadNews(url,fileName,postCount);
       }
     }
     xhttp.send();
   },
   
+  //parameters are provided by loadNews()
   getNews: function(url,fileName,postCount) {
+    //News Setup
+    let temp = document.createElement('iframe');
+    temp.className = 'newsPost';
+    temp.style = 'border:none;';
+    temp.scrolling = 'no';
+    temp.height = '0';
+    
+    let iframe = document.querySelector('#adventureNews').insertBefore(temp,document.querySelector('#adventureNews').childNodes[0]);
+    
     for (let i=1;i<=5;i++) {
-      let temp = document.createElement('iframe');
-      temp.className = 'newsPost';
-      temp.style = 'border:none;';
-      temp.scrolling = 'no';
-      temp.height = '0';
-      
-      let iframe = document.querySelector('#adventureNews').insertBefore(temp,document.querySelector('#adventureNews').childNodes[0]);
-      
-      //sets all iframes under #adventureNews to their individual content heights.
+      //sets all iframes under #adventureNews to their individual content heights when loaded.
       iframe.onload = function() {
         if (!RegExp('Page not found').test(this.contentDocument.title)) {
           this.height = this.contentWindow.document.body.scrollHeight;
         }
       }
-      //grabs text files from url param
+      //grabs text files from parameters
       iframe.src = url+(postCount-i+1)+fileName+'.html';
     }
   }
