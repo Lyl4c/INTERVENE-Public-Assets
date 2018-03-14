93938fae3e8a1228643ee99f564afd781b9a6f16
SickHippie <sickhippie@sickhippie.com>
2018-03-13 22:29:44 -0700

    Beautified

--- Mirror/MSPA-6-15-09/MSPA 6-15-09 launch.js
+++ Mirror/MSPA-6-15-09/MSPA 6-15-09 launch.js
@@ -2,21 +2,35 @@
 //https://github.com/Lyl4c/INTERVENE-Public-Assets
 //https://web.archive.org/web/20090619195506/http://www.mspaintadventures.com:80/?s=6&p=002154
 var offSite = 'https://lyl4c.github.io/INTERVENE-Public-Assets/'
-function newDiv(x) {return document.createElement(x);}
-function getByClass(x,y=0) {return document.querySelectorAll(x)[y];}
+
+function newDiv(x) {
+    return document.createElement(x);
+}
+
+function getByClass(x, y = 0) {
+    return document.querySelectorAll(x)[y];
+}
+
 function setDivAtt(x) {
     let temp = new newDiv(x.div);
     delete x.div;
     for (let i in x) {
-        temp.setAttribute(i,x[i]);
+        temp.setAttribute(i, x[i]);
     }
     return temp;
 }
 //x = target div | y = child target | z = new Div
-function insertDiv(x,y,z){return getByClass(x).insertBefore(z,getByClass(x).childNodes[y]);}
-function removeDiv(x,y){return getByClass(x).removeChild(getByClass(x).childNodes[y]);}
+function insertDiv(x, y, z) {
+    return getByClass(x).insertBefore(z, getByClass(x).childNodes[y]);
+}
+
+function removeDiv(x, y) {
+    return getByClass(x).removeChild(getByClass(x).childNodes[y]);
+}
 //x.xc = target old div.child | y.yc = new div.child
-function cloneDiv(x,xc,y,yc){return getByClass(y).insertBefore(getByClass(x).childNodes[xc].cloneNode(true),getByClass(y).childNodes[yc]);}
+function cloneDiv(x, xc, y, yc) {
+    return getByClass(y).insertBefore(getByClass(x).childNodes[xc].cloneNode(true), getByClass(y).childNodes[yc]);
+}
 /*
 //unused function
 //sets target div type to new dive type. cannot set target div to its current div type
@@ -32,8 +46,16 @@ function setDivType(x,y,z){
 }
 */
 //insert mspa banner above #slide
-insertDiv('#main #container',0,setDivAtt({'div':'a','href':'http://www.mspaintadventures.com'}));
-insertDiv('#container a',0,setDivAtt({'div':'img','height':'initial','width':'initial','src':offSite+'Mirror/MSPA-6-15-09/images/mspa-logo.gif'}));
+insertDiv('#main #container', 0, setDivAtt({
+    'div': 'a',
+    'href': 'http://www.mspaintadventures.com'
+}));
+insertDiv('#container a', 0, setDivAtt({
+    'div': 'img',
+    'height': 'initial',
+    'width': 'initial',
+    'src': offSite + 'Mirror/MSPA-6-15-09/images/mspa-logo.gif'
+}));
 /*
 //move comment box to #slide
 cloneDiv('#container #info',1,'#container #slide',5);
@@ -45,31 +67,40 @@ insertDiv('#commentbox div div',0,setDivAtt({'div':'input','type':'image','src':
 insertDiv('#slide #foot',0,setDivAtt({'div':'br'}));
 */
 //move footer umcontainer above #info
-cloneDiv('#main footer',1,'#main #container',3);
-removeDiv('#main footer',1);
+cloneDiv('#main footer', 1, '#main #container', 3);
+removeDiv('#main footer', 1);
 
 //move latestpages above #infobox 
-insertDiv('#container #info',0,setDivAtt({'div':'span','id':'latestpages'}));
-cloneDiv('#infobox #latestpages .spoiler',1,'#info #latestpages',0);
-removeDiv('#info #infobox tr',2);
+insertDiv('#container #info', 0, setDivAtt({
+    'div': 'span',
+    'id': 'latestpages'
+}));
+cloneDiv('#infobox #latestpages .spoiler', 1, '#info #latestpages', 0);
+removeDiv('#info #infobox tr', 2);
 
 //trim string length in #latestpages
-for (let i=0;i<document.querySelectorAll('#latestpages div > span').length;i++) {
-    if (getByClass('#latestpages a > span',i).innerHTML.length > 25) {
-        getByClass('#latestpages a > span',i).innerHTML = getByClass('#latestpages a > span',i).innerHTML.slice(0,25)+'...';
+for (let i = 0; i < document.querySelectorAll('#latestpages div > span').length; i++) {
+    if (getByClass('#latestpages a > span', i).innerHTML.length > 25) {
+        getByClass('#latestpages a > span', i).innerHTML = getByClass('#latestpages a > span', i).innerHTML.slice(0, 25) + '...';
     }
 }
 
 //delete adventure info spoiler boxes, move adventure info #adventureinfo
-insertDiv('#info #infobox',0,setDivAtt({'div':'div','id':'adventureinfo'}));
-cloneDiv('#infobox .spoiler div:last-of-type',0,'#infobox #adventureinfo',0);
-removeDiv('#info #infobox',1);
+insertDiv('#info #infobox', 0, setDivAtt({
+    'div': 'div',
+    'id': 'adventureinfo'
+}));
+cloneDiv('#infobox .spoiler div:last-of-type', 0, '#infobox #adventureinfo', 0);
+removeDiv('#info #infobox', 1);
 
 //add news image for #adventureNews
 //insertDiv('#info #infobox',1,setDivAtt({'div':'img','height':'initial','width':'initial','src':offSite+'Mirror/MSPA-6-15-09/images/newsimg.gif'}));
 
 //fake copyright image. its here to mimic the real copyright image used in ye olde mspaintadventures times
-insertDiv('#main footer',0,setDivAtt({'div':'img','src':offSite+'Mirror/MSPA-6-15-09/images/notcopyright.gif'}));
+insertDiv('#main footer', 0, setDivAtt({
+    'div': 'img',
+    'src': offSite + 'Mirror/MSPA-6-15-09/images/notcopyright.gif'
+}));
 /*
 //news setup
 MSPFA.import('/js/?s=24896',function(){
--- News/newsPosts.js
+++ News/newsPosts.js
@@ -6,59 +6,59 @@ var Lylac = {
   //Initial setup
   //STRING selector = target div | The parent div of where you want to put your posts.
   //NUMBER selectorChild = child div of target div | Where you actually want to place your posts.
-  setupNews: function(selector,selectorChild=0) {
+  setupNews: function (selector, selectorChild = 0) {
     let temp = document.createElement('div');
     temp.id = 'adventureNews';
-    document.querySelector(selector).insertBefore(temp,document.querySelector(selector).childNodes[selectorChild]);
+    document.querySelector(selector).insertBefore(temp, document.querySelector(selector).childNodes[selectorChild]);
   },
-  
+
   //News Post Crawler; Returns # of posts found in url param
   //STRING url = web address | external source of your News Posts.
   //STRING fileName = name of the file | example: -intervene | <- use this instead of the full name: 71-intervene.html.
   //                                     The file MUST be an HTML file.
   //NUMBER postCount = starting number of the crawler code | can be used to skip post entries.
-  loadNews: function(url,fileName,postCount=0) {
-    xhttp.open('GET', url+(postCount+1)+fileName+'.html', true);
-    xhttp.onreadystatechange = function() {
-      if(xhttp.readyState == XMLHttpRequest.DONE) {
+  loadNews: function (url, fileName, postCount = 0) {
+    xhttp.open('GET', url + (postCount + 1) + fileName + '.html', true);
+    xhttp.onreadystatechange = function () {
+      if (xhttp.readyState == XMLHttpRequest.DONE) {
         if (xhttp.status) {
-          statusType = Math.floor(xhttp.status/100);
-          if(statusType == 2) {
+          statusType = Math.floor(xhttp.status / 100);
+          if (statusType == 2) {
             postCount++;
           } else if (statusType == 4) {
-            console.log('Number of Posts = '+postCount);
-            Lylac.getNews(url,fileName,postCount);
+            console.log('Number of Posts = ' + postCount);
+            Lylac.getNews(url, fileName, postCount);
             return;
             //Recursion loop stops here.
           }
         }
         //recursion loop
-        Lylac.loadNews(url,fileName,postCount);
+        Lylac.loadNews(url, fileName, postCount);
       }
     }
     xhttp.send();
   },
-  
+
   //parameters are provided by loadNews()
-  getNews: function(url,fileName,postCount) {
-    for (let i=5;i>=1;i--) {
+  getNews: function (url, fileName, postCount) {
+    for (let i = 5; i >= 1; i--) {
       //News Setup
       let temp = document.createElement('iframe');
       temp.className = 'newsPost';
       temp.style = 'border:none;';
       temp.scrolling = 'no';
       temp.height = '0';
-      
-      let iframe = document.querySelector('#adventureNews').insertBefore(temp,document.querySelector('#adventureNews').childNodes[0]);
-      
+
+      let iframe = document.querySelector('#adventureNews').insertBefore(temp, document.querySelector('#adventureNews').childNodes[0]);
+
       //sets all iframes under #adventureNews to their individual content heights when loaded.
-      iframe.onload = function() {
+      iframe.onload = function () {
         if (!RegExp('Page not found').test(this.contentDocument.title)) {
           this.height = this.contentWindow.document.body.scrollHeight;
         }
       }
       //grabs text files from parameters
-      iframe.src = url+(postCount-i+1)+fileName+'.html';
+      iframe.src = url + (postCount - i + 1) + fileName + '.html';
     }
   }
 }
