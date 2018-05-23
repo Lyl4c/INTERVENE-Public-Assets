c8da4a714ddf5cdeaaca7a9a884e37cbf2c7c382
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-05-23 01:01:27 -0700

    1.2.48 added dev environment

--- News/newsPosts2.js
+++ News/newsPosts2.js
@@ -1,3 +1,7 @@
 //News Post Script made by Lylac_SYS
 //Assistance provided by: sickhippie, Miroware
-window.postMessage(console.log('Hello'),'https://mspfa.com');
+var Lylac = {
+  Test: function () {
+    window.postMessage(console.log('Hello'),'https://mspfa.com');
+  }
+}
