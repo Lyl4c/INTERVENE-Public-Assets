0046d9170d007f4ce2d345f43d42d21841b5faf6
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-05-24 08:04:21 -0700

    1.2.57

--- News/controller.js
+++ News/controller.js
@@ -1,14 +1,6 @@
 //News Post Script made by Lylac_SYS
 //Assistance provided by: sickhippie, Miroware
 //window.postMessage(console.log('Hello'),'https://mspfa.com');
-
-window.addEventListener("message", receiveMessage, false);
-
-function receiveMessage(event)
-{
-  if (event.origin !== "https://lyl4c.github.io") {
-    return;
-  } else {
-    console.log('Hello');
-  }
-}
+(function() {
+  console.log('Hello');
+})();
