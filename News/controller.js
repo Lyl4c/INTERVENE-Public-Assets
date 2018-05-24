2ef9b4c92c629c96ee4b763b62d03f5e9eb70afd
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-05-24 05:47:34 -0700

    1.2.51 Patience is a virtue
    The next bunch of commits will just be focused on finding out what works and what doesn't.

---
+++ News/controller.js
@@ -0,0 +1,3 @@
+//News Post Script made by Lylac_SYS
+//Assistance provided by: sickhippie, Miroware
+window.postMessage(console.log('Hello'),'https://mspfa.com');
--- News/newsPosts2.js
+++
@@ -1,7 +0,0 @@
-//News Post Script made by Lylac_SYS
-//Assistance provided by: sickhippie, Miroware
-var Lylac = {
-  Test: function () {
-    window.postMessage(console.log('Hello'),'https://mspfa.com');
-  }
-}
