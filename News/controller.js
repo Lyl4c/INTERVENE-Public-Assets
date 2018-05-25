54611b05ca31634b8fa770072ec76ea29cf54714
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2018-05-24 19:07:34 -0700

    1.2.52

--- News/controller.js
+++ News/controller.js
@@ -1,3 +1,3 @@
 //News Post Script made by Lylac_SYS
 //Assistance provided by: sickhippie, Miroware
-window.postMessage(console.log('Hello'),'https://mspfa.com');
+window.postMessage(function(){console.log('Hello')},'https://mspfa.com');
